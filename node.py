6916dbfbe06881096ac28c2aa853216f64f5b384
zw <zhaw@pku.edu.cn>
2016-12-03 00:38:38 +0800

    little progress

--- node.py
+++ node.py
@@ -1,50 +1,56 @@
 import mxnet as mx
 import numpy as np
 
+N_STATES = 13
+N_PLAYERS = 2
+
+class PolicyMaker():
+    # PolicyMaker needs to return n_choices nd-array of shape (N_STATES,).
+    def __init__(self, n_choices, feature, name):
+        self.name = name
+        self.n_choices = n_choices
+        self.args = [mx.sym.Variable('%s_pm'%name)]
+        self.out = mx.sym.SoftmaxActivation(self.args[0], mode='instance') # n_states * n_choices
+        self.out = mx.sym.SliceChannel(self.out, num_outputs=n_choices, axis=1, squeeze_axis=True)
+
+    def infer_shape(self):
+        self.shapes = [(N_STATES, self.n_choices)]
+
+
 class Node(object):
     def __init__(self):
         pass
 
     def register_parent(self, upper, ith_child):
         self.parent = upper
-        self.prob_in = upper.outputs[ith_child]
+        self.pin = upper.outputs[ith_child]
         upper.children[ith_child] = self
+        self.pot = upper.pot + upper.cost[ith_child]
+        self.board = upper.board + []
 
     def alloc_children(self):
-        self.outputs = [] 
-        self.grad = []
+        self.pout = [] 
         self.children = []
         for i in range(self.n_children):
             tmp = []
-            gtmp = []
-            for j in range(n_players):
-                tmp.append(np.ones([1, n_states]) * 1./n_states)
-                gtmp.append(np.ones([1, n_states]) * 1./n_states)
-            self.outputs.append(tmp)
-            self.grad.append(gtmp)
+            for j in range(N_PLAYERS):
+                tmp.append([])
+            self.pout.append(tmp)
             self.children.append([])
-
-    def forward(self):
-        pass
-
-    def backward(self):
-        pass
 
 
 class RootNode(Node):
-    def __init__(self, n_players, n_states, name):
-        self.probs = []
+    def __init__(self, n_states, name):
+        self.pin = []
         self.n_children = 1
-        for j in range(n_players):
-            self.probs.appned(np.ones([1, n_states]) * 1./n_states)
+        for j in range(N_PLAYERS):
+            self.pin.append(mx.sym.Variable('player%d_prob'%j)) # should be np.ones([1, n_states]) * 1./n_states
         super(RootNode, self).alloc_children()
-
-    def forward(self):
-        pass
-
-    def backward(self):
-        pass
-
+        for j in range(N_PLAYERS):
+            self.pout[j] = self.pin
+        self.board = []
+        self.pot = 0
+        self.loss = [0 for i in range(N_PLAYERS)]
 
 
 class DecisionNode(Node):
@@ -57,31 +63,21 @@ class DecisionNode(Node):
         cost:           cost of each decision
         name:           node's name
         '''
+        self.name = name
+        self.cost = cost
+        self.player = ith_player
         super(DecisionNode, self).register_parent(upper, ith_child)
         self.n_children = n_children
+        self.policy_maker = PolicyMaker(n_children, [], self.name) 
         super(DecisionNode, self).alloc_children(self)
-        self.get_policy_maker()
-        self.make_graph()
-        self.player = ith_player
+        for i in range(self.n_children):
+            for j in range(N_PLAYERS):
+                if j == self.player:
+                    self.pout[j] = self.policy_maker.out[i] * self.pin[j]
+                else:
+                    self.pout[j] = self.pin[j]
 
-    def get_policy_maker(self):
-        pass
-
-    def make_graph(self):
-
-    def forward(self):
-        '''
-        FORWARD THE POLICY MAKER AND GET THE PROB OF EACH MOVE
-        COMPUTE PROBABILITY AT EACH CHILDREN NODE
-        ACCUMULATE COST
-        '''
-        policy = self.policy_maker.forward()
-
-    def backward(self):
-        '''
-        BACKPROPAGATE THE DECISION MAKER
-        PASS THE GRADIENT UP
-        '''
+    def update_feature(self):
         pass
 
 
@@ -91,7 +87,11 @@ class RevealNode(Node):
     # When forward, RevealNode provide a sample of board states
     # When backward, RevealNode does nothing
     def __init__(self, upper, ith_child, name):
+        self.cost = [0] * N_PLAYERS
+        super(RevealNode, self).register_parent(upper, ith_child)
         super(RevealNode, self).address_children(upper, ith_child)
+        for j in range(N_PLAYERS):
+            self.pout[j] = self.pin
 
     def forward(self):
         '''
@@ -108,16 +108,18 @@ class RevealNode(Node):
 
 class AwardNode(Node):
     def __init__(self, upper, ith_child, winner, name):
+        self.cost = [0] * N_PLAYERS
         super(AwardNode, self).address_children(upper, ith_child)
         self.winner = winner 
 
 class ShowdownNode(Node):
     def __init__(self, upper, ith_child, name):
+        self.cost = [0] * N_PLAYERS
         super(ShowdownNode, self).address_children(upper, ith_child)
 
     def forward(self):
         '''
-        COMPUTE AWARD ACCORDING TO DISTRIBUTION OF TWO PLAYERS HAND
+        COMPUTE AWARD ACCORDING TO DISTRIBUTION OF TWO PLAYERS HAND AND THE BOARD
         '''
         pass
 
