3a48907ddba89e74e97b447b3d996a7a60cfa933
zhaw <zhaw@pku.edu.cn>
2016-12-05 22:40:54 +0800

    fix bugs

--- node.py
+++ node.py
@@ -59,10 +59,11 @@ class PolicyMaker():
             self.executor.outputs[i].copyto(graph.arg_dict['%s_%d_policy'%(self.name,i)])
 
     def update(self, grad_list):
-        s = mx.nd.zeros(grad_list[0].shape, CTX)
+        s = mx.nd.zeros([1], CTX)
         for v in grad_list:
-            s += mx.nd.abs(v)
-        s = mx.nd.maximum(s, mx.nd.ones(s.shape, CTX)*1e-20)
+            s += mx.nd.sum(mx.nd.abs(v))
+        s = mx.nd.maximum(s, mx.nd.ones([1], CTX)*1e-30)
+        s = s.asnumpy()[0]
         self.executor.backward([-v/s for v in grad_list])
         for i, k in enumerate(self.executor.grad_dict):
             if k[1] == '+':
@@ -72,7 +73,9 @@ class PolicyMaker():
         mx.nd.save('%s/%s.nd'%(folder, self.name), self.executor.arg_dict)
 
     def load(self, folder):
-        self.executor.arg_dict = mx.nd.load('%s/%s.nd'%(folder, self.name))
+        pretrained = mx.nd.load('%s/%s.nd'%(folder, self.name))
+        for k in pretrained:
+            pretrained[k].copyto(self.executor.arg_dict[k])
 
 
 class Node(object):
@@ -152,8 +155,8 @@ class RootNode(Node):
         cc2 = []
         for i in range(BATCH_SIZE):
             x = np.random.choice(range(len(CARDS)), N_PLAYERS+N_BOARD, replace=False)
-#            x[0] = 0
-#            x[1] = 1
+#            x[0] = i%26
+#            x[1] = 3
 #            x[2] = 2
             cc0.append(CARDS[x[0]][1])
             cc1.append(CARDS[x[1]][1])
@@ -175,6 +178,33 @@ class RootNode(Node):
                     tmp[i, k, samples[i][j+N_PLAYERS][0], samples[i][j+N_PLAYERS][1]] = 1
             self.arg_dict['board%d'%j][:] = tmp
         return np.array(map(get_win_share, samples)), cc0, cc1, cc2
+    
+
+    def manual(self):
+        def get_win_share(s):
+            if s[0][1] == s[2][1]:
+                return [1, 0]
+            if s[1][1] == s[2][1]:
+                return [0, 1]
+            if s[0][1] == s[1][1]:
+                return [0.5, 0.5]
+            if s[0][1] > s[1][1]:
+                return [1, 0]
+            if s[0][1] < s[1][1]:
+                return [0, 1]
+        code = eval(raw_input("cards?\n"))
+        samples = [code for i in range(BATCH_SIZE)]
+        for j in range(N_PLAYERS):
+            tmp = np.zeros(self.arg_dict['player%d_hand'%j].shape)
+            for k in range(1):
+                tmp[0, k, code[j][0], code[j][1]] = 1
+            self.arg_dict['player%d_hand'%j][:] = tmp
+        for j in range(N_BOARD):
+            tmp = np.zeros(self.arg_dict['board%d'%j].shape)
+            for k in range(1):
+                tmp[0, k, code[j+N_PLAYERS][0], code[j+N_PLAYERS][1]] = 1
+            self.arg_dict['board%d'%j][:] = tmp
+        return np.array(map(get_win_share, samples))
 
 
 class DecisionNode(Node):
@@ -365,6 +395,107 @@ def train():
         if count % 1000 == 0:
             for dnode in param_nodes:
                 dnode.policy_maker.dump('model')
+
+def test():
+    root = RootNode('root')
+
+    pf1d = DecisionNode(root, 0, 0, 3, [2,1,0], 'pf1d')
+    flrv1 = RevealNode(pf1d, 1, [2], 'flrv1')
+    fl1d1 = DecisionNode(flrv1, 0, 0, 3, [1,0,0], 'fl1d1')
+    fl2f1 = AwardNode(fl1d1, 2, 1, 'fl2f1')
+    fl2s1 = ShowdownNode(fl1d1, 1, 'fl2s1')
+    fl2d1 = DecisionNode(fl1d1, 0, 1, 3, [2,1,0], 'fl2d1')
+    fl3f1 = AwardNode(fl2d1, 2, 0, 'fl3f1')
+    fl3s1 = ShowdownNode(fl2d1, 1, 'fl3s1')
+    fl3d1 = DecisionNode(fl2d1, 0, 0, 3, [2,1,0], 'fl3d1')
+    fl4f1 = AwardNode(fl3d1, 2, 1, 'fl4f1')
+    fl4s1 = ShowdownNode(fl3d1, 1, 'fl4s1')
+    fl4d1 = DecisionNode(fl3d1, 0, 1, 2, [1,0], 'fl4d1')
+    fl5f1 = AwardNode(fl4d1, 1, 0, 'fl5f1')
+    fl5s1 = ShowdownNode(fl4d1, 0, 'fl5s1')
+    pf2f = AwardNode(pf1d, 2, 1, 'pf2f')
+
+    pf2d = DecisionNode(pf1d, 0, 1, 3, [2,1,0], 'pf2d')
+    flrv2 = RevealNode(pf2d, 1, [2], 'flrv2')
+    fl1d2 = DecisionNode(flrv2, 0, 0, 3, [1,0,0], 'fl1d2')
+    fl2f2 = AwardNode(fl1d2, 2, 1, 'fl2f2')
+    fl2s2 = ShowdownNode(fl1d2, 1, 'fl2s2')
+    fl2d2 = DecisionNode(fl1d2, 0, 1, 3, [2,1,0], 'fl2d2')
+    fl3f2 = AwardNode(fl2d2, 2, 0, 'fl3f2')
+    fl3s2 = ShowdownNode(fl2d2, 1, 'fl3s2')
+    fl3d2 = DecisionNode(fl2d2, 0, 0, 3, [2,1,0], 'fl3d2')
+    fl4f2 = AwardNode(fl3d2, 2, 1, 'fl4f2')
+    fl4s2 = ShowdownNode(fl3d2, 1, 'fl4s2')
+    fl4d2 = DecisionNode(fl3d2, 0, 1, 2, [1,0], 'fl4d2')
+    fl5f2 = AwardNode(fl4d2, 1, 0, 'fl5f2')
+    fl5s2 = ShowdownNode(fl4d2, 0, 'fl5s2')
+    pf3f = AwardNode(pf2d, 2, 0, 'pf3f')
+
+    pf3d = DecisionNode(pf2d, 0, 0, 2, [1,0], 'pf3d')
+    flrv3 = RevealNode(pf3d, 0, [2], 'flrv3')
+    fl1d3 = DecisionNode(flrv3, 0, 0, 3, [1,0,0], 'fl1d3')
+    fl2f3 = AwardNode(fl1d3, 2, 1, 'fl2f3')
+    fl2s3 = ShowdownNode(fl1d3, 1, 'fl2s3')
+    fl2d3 = DecisionNode(fl1d3, 0, 1, 3, [2,1,0], 'fl2d3')
+    fl3f3 = AwardNode(fl2d3, 2, 0, 'fl3f3')
+    fl3s3 = ShowdownNode(fl2d3, 1, 'fl3s3')
+    fl3d3 = DecisionNode(fl2d3, 0, 0, 3, [2,1,0], 'fl3d3')
+    fl4f3 = AwardNode(fl3d3, 2, 1, 'fl4f3')
+    fl4s3 = ShowdownNode(fl3d3, 1, 'fl4s3')
+    fl4d3 = DecisionNode(fl3d3, 0, 1, 2, [1,0], 'fl4d3')
+    fl5f3 = AwardNode(fl4d3, 1, 0, 'fl5f3')
+    fl5s3 = ShowdownNode(fl4d3, 0, 'fl5s3')
+    pf4f = AwardNode(pf3d, 1, 1, 'pf4f')
+
+    leaf_nodes = [pf2f, pf3f, pf4f,
+                    fl2s1, fl2f1, fl3s1, fl3f1, fl4s1, fl4f1, fl5s1, fl5f1,
+                    fl2s2, fl2f2, fl3s2, fl3f2, fl4s2, fl4f2, fl5s2, fl5f2,
+                    fl2s3, fl2f3, fl3s3, fl3f3, fl4s3, fl4f3, fl5s3, fl5f3
+                    ]
+    showdown_nodes = [fl2s1, fl3s1, fl4s1, fl5s1,
+                    fl2s2, fl3s2, fl4s2, fl5s2,
+                    fl2s3, fl3s3, fl4s3, fl5s3
+                    ]
+                    
+    param_nodes = [pf1d, pf2d, pf3d,
+                    fl1d1, fl2d1, fl3d1, fl4d1,
+                    fl1d2, fl2d2, fl3d2, fl4d2,
+                    fl1d3, fl2d3, fl3d3, fl4d3
+                    ]
+    outs = map(lambda x:x.pout[0], leaf_nodes)
+    args = {}
+    args.update(root.arg_dict)
+    for node in param_nodes:
+        args.update(node.arg_dict)
+    grad_dict = {}
+    for k in args:
+        if not k.startswith('player') and not k.startswith('board') and not k.startswith('root'):
+            grad_dict[k] = mx.nd.zeros(args[k].shape, CTX)
+
+    final_out = mx.sym.Group(outs)
+    graph = final_out.bind(ctx=CTX, args=args, args_grad=grad_dict, grad_req='write')
+    for dnode in param_nodes:
+        dnode.policy_maker.get_executor(root.arg_dict)
+    for dnode in param_nodes:
+        dnode.policy_maker.load('model')
+
+    while True:
+        win_shares = root.manual()
+        for node in showdown_nodes:
+            node.share[:] = win_shares
+        for j in range(N_PLAYERS):
+            for node in leaf_nodes:
+                node.get_grad(j)
+            for dnode in param_nodes:
+                dnode.policy_maker.forward(graph)
+                print dnode.name, [graph.arg_dict['%s_%d_policy'%(dnode.name, i)].asnumpy()[0] for i in range(dnode.n_children)]
+            graph.forward(is_train=True)
+            graph.backward([-node.grad for node in leaf_nodes])
+            for dnode in param_nodes:
+                if dnode.name[0] == str(j):
+                    print j, dnode.name, [graph.grad_dict['%s_%d_policy'%(dnode.name, i)].asnumpy()[0]\
+                            / max(1e-30, np.sum(np.abs(graph.grad_dict['%s_%d_policy'%(dnode.name,i)].asnumpy()[0]))) for i in range(dnode.n_children)]
+
 
 if __name__ == '__main__':
     train()
